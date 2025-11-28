bf0e07f84b34b4e43ac7979c607d57a3fc51eb58
Muhammad Faseeh Ur Rehman <faseehurrehman241@gmail.com>
2025-11-28 20:41:21 +0500

    Initial Spring Boot Project

---
+++ src/main/java/com/company/fyp_management/FypManagementApplication.java
@@ -0,0 +1,13 @@
+package com.company.fyp_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FypManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FypManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/fyp_management/FypManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.fyp_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FypManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
